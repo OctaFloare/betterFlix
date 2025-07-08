d0c06d1875f727bf7aa7b86edee0a1e56e594d3d
Floare Octavian <adifloare1969@gmail.com>
2025-07-08 13:16:05 +0300

    [master] Initial commit

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,11 @@
+import { Movies } from "../components/movies";
+
+
+const MoviesPage = () => {
+    return <>
+        <h1 className="flex self-center justify-center pb-20 text-5xl">Movies</h1>
+       <Movies /> 
+    </>
+};
+
+export default MoviesPage
