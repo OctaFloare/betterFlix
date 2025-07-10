36c3b2e952663284a4f9f6ea7a9b9b716e48bc77
Floare Octavian <adifloare1969@gmail.com>
2025-07-10 10:06:12 +0300

    [carousel] Fix issue with all elements displayed on screen

--- src/app/components/movies/carousel.tsx
+++ src/app/components/movies/carousel.tsx
@@ -2,23 +2,28 @@ import { Movie } from "../types"
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { MovieCard } from "./movieCard"
+import { useEffect } from "react"
 
 type Props = {
     movies?: Movie[]
 }
 
 export const Carousel = ({ movies }: Props) => {
-    const [sliderRef] = useKeenSlider({
-        slides: {
-            perView: 5,
-            spacing: 8
-        },
+
+    const [sliderRef, instanceRef] = useKeenSlider({
+        slides: { perView: 5, spacing: 8 },
     });
 
-    return <div ref={sliderRef} className="keen-slider overflow-visible!">
+      useEffect(() => {
+    if (instanceRef.current) {
+      instanceRef.current.update();
+    }
+  }, [movies]);
+
+    return <div ref={sliderRef} className="keen-slider">
         {!!movies && movies.map((movie, index) => {
             return <div key={`${movie.title}-${index}`}
-                        className={`keen-slider__slide number-slide${index + 1} overflow-visible! z-1`}>
+                        className={`keen-slider__slide number-slide${index + 1} z-1`}>
                 <MovieCard movie={movie}/>
             </div>
         })}
