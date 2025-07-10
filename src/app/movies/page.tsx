83c51351683388d052dc75af6de545e93516d6d6
4ndreiOp <andrei.oprescu.2404@gmail.com>
2025-07-10 11:52:28 +0300

    nav-fix

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,10 +1,8 @@
 import { Movies } from "../components/movies";
-import { Navigation } from "../components/navigation";
 
 
 const MoviesPage = () => {
     return <>
-        <Navigation />
         <h1 className="flex self-center justify-center pb-20 pt-5 text-5xl">Movies</h1>
         <Movies />
     </>
