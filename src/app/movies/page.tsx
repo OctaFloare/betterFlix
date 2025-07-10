b01adebe2446fdf2be8b82b4fe4c3e3ebb7d8e7b
4ndreiOp <andrei.oprescu.2404@gmail.com>
2025-07-10 14:00:01 +0300

    4ndrei op's master  (#6)
    * Add navigation component +some style tweaks
    
    * nav-fix
    
    * POST-Create Movie

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -3,8 +3,8 @@ import { Movies } from "../components/movies";
 
 const MoviesPage = () => {
     return <>
-        <h1 className="flex self-center justify-center pb-20 text-5xl">Movies</h1>
-       <Movies /> 
+        <h1 className="flex self-center justify-center pb-20 pt-5 text-5xl">Movies</h1>
+        <Movies />
     </>
 };
 
